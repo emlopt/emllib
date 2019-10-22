05985e084c07c2b159f9ebf769502410d321e7a2
Federico Baldo <federicobaldo@Federicos-MacBook-Pro.local>
2019-10-22 16:25:52 +0200

    added readme reference in documentation

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -10,7 +10,7 @@ Welcome to emllib's documentation!
    :maxdepth: 2
    :caption: Contents:
 
-
+    readme
 
 Indices and tables
 ==================
--- eml/tree/reader/sklearn_reader.py
+++ eml/tree/reader/sklearn_reader.py
@@ -5,6 +5,47 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from eml.tree import describe
 
+""" 
+IMPORTANT 
+
+In order to embed a decision tree model we must take into account 
+the way the thresholds are testes by the model. 
+
+Example:
+
+    scikit learn tests the threshold for each attribute considering 
+
+        th_1 <= x < th_2
+
+    every time the equality will be tested on the "left" threshold.
+
+    We might have some models, for example Weka, where the equality 
+    test is made on the right side of the range. 
+
+To make stuff more simple we established a convention for this model, 
+where the equality test is always considered to be on the left side. 
+
+This can be actually achived very easily using the machine epsilon,
+upper bound on the relative error due to rounding in floating point 
+arithmetic, by "shifting" the equality test from right to left summing
+this value
+
+Example
+
+    let us consider a model where the equality test is made on the right-
+    hand of the range:
+
+        th_1 < x <= th_2
+
+    we can change this as follows
+
+        th_1 + epsilon < x <= th_2 - epsilon    <=>
+
+        <=> th_1 <= x < th_2
+
+This still needs to be implemented
+"""
+
 
 # give one of feature_list or feature_dict to map it to the names
 def read_sklearn_tree(tree):
@@ -26,47 +67,6 @@ def read_sklearn_tree(tree):
     """ 
     root = describe.DTNode()
     return _sklearn_tree_export(tree.tree_, 0, root)
-
-    """ 
-    IMPORTANT 
-
-    In order to embed a decision tree model we must take into account 
-    the way the thresholds are testes by the model. 
-
-    Example:
-
-        scikit learn tests the threshold for each attribute considering 
-
-            th_1 <= x < th_2
-
-        every time the equality will be tested on the "left" threshold.
-
-        We might have some models, for example Weka, where the equality 
-        test is made on the right side of the range. 
-
-    To make stuff more simple we established a convention for this model, 
-    where the equality test is always considered to be on the left side. 
-    
-    This can be actually achived very easily using the machine epsilon,
-    upper bound on the relative error due to rounding in floating point 
-    arithmetic, by "shifting" the equality test from right to left summing
-    this value
-
-    Example
-
-        let us consider a model where the equality test is made on the right-
-        hand of the range:
-
-            th_1 < x <= th_2
-
-        we can change this as follows
-
-            th_1 + epsilon < x <= th_2 - epsilon    <=>
-
-            <=> th_1 <= x < th_2
-
-    This still needs to be implemented
-    """
 
 
 def _sklearn_tree_export(tree, nid, root):
